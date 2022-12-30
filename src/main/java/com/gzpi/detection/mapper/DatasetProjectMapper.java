1dc7b843022712ebe1ae9a1ac0e52254dc8dde53
GaoZhenGit <912815282@qq.com>
2022-12-30 16:56:16 +0800

    更换mapper位置

--- src/main/java/com/gzpi/detection/mapper/DatasetProjectMapper.java
+++ src/main/java/com/gzpi/detection/mapper/DatasetProjectMapper.java
@@ -1,14 +1,20 @@
 package com.gzpi.detection.mapper;
 
 import com.gzpi.detection.bean.DatasetProject;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface DatasetProjectMapper {
+    @Select("SELECT id, name, type, clip_size as clipSize FROM project WHERE id=#{id}")
     DatasetProject getProjectById(String id);
+
+    @Select("SELECT id, name, type, clip_size as clipSize FROM project")
     List<DatasetProject> getAllProjects();
+
+    @Insert("insert into project(id, name, type, clip_size) VALUES (#{id}, #{name}, #{type}, #{clipSize})")
     void save(DatasetProject project);
+    @Delete("delete from project WHERE id=#{id}")
     void delete(String id);
 }
